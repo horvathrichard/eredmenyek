353a732aa2b904ee584f1a30d8b8d2179373756a
ricsi <horvath.richard.jozsef_2020c@bolyai-szakkozep.hu>
2024-04-18 09:29:09 +0200

    Add project files.

---
+++ Personrepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace datagrid2
+{
+    class EredmenyRepository
+    {
+        public string Path { get; set; }
+        public char Delimiter { get; set; }
+        public bool SkipHeader { get; set; }
+
+        public List<Eredmeny> FindAll()
+        {
+            StreamReader r = new StreamReader(this.Path, true);
+            if (this.SkipHeader) { r.ReadLine(); }
+
+            List<Eredmeny> eredmenyListOutput = new List<Eredmeny> { };
+
+            while (!r.EndOfStream)
+            {
+                eredmenyListOutput.Add(new Eredmeny(r.ReadLine()));
+            }
+            return eredmenyListOutput;
+        }
+    }
+}
